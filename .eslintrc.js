9cfb25b6119085ba96fa8720820a576e6426f204
med zino <medzino85@gmail.com>
2025-01-03 23:41:33 +0000

    setting the out folder to deploy

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['next/core-web-vitals'],
+    rules: {
+      // Disable specific rules
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'off',
+      'no-console': 'off',
+      // Add other rules here
+    },
+  };
+  
